1acd968ce7c35df9e5785cc44de809f746551c8e
Evgenii <zvolev@mail.ru>
2022-10-06 23:28:37 +0300

    добавил 3ю задачу модернизированную под разные условия

--- DZ3/Program.cs
+++ DZ3/Program.cs
@@ -3,3 +3,42 @@
 // 645 -> 5
 // 78 -> третьей цифры нет
 // 326792 -> 6
+// адаптировал код под разные запросы (не только третья цифра)
+
+Console.Clear();
+
+Console.WriteLine("Введите любое целое число");
+int a = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите порядковый номер цифры, которую надо вывести на экран");
+int count = Convert.ToInt32(Console.ReadLine());
+
+int NumLength(int num) // считаем сколько цифр в числе
+{
+    int index = 1;
+    while (num > 10)
+    {
+        num = num / 10;
+        index++;
+    }
+    return index;
+}
+
+int numLength = NumLength(a);
+
+if (numLength >= count)
+{
+    int index = 0;
+    
+    while (numLength > (count + index))
+    { 
+        a = a / 10;
+        index ++;
+    }
+    a = a % 10;
+    Console.WriteLine($"{count}-я цифра {a}");
+}
+else Console.WriteLine($"{count}-й цифры нет!");
+
+
+
